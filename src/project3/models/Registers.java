9ebb36e943d03a7a24d5307ec03f3e7ef4d8bb83
Ethan Johnson <evj@g.clemson.edu>
2023-04-10 14:39:17 -0400

    finished barebones registers

--- src/project3/models/Registers.java
+++ src/project3/models/Registers.java
@@ -1,5 +1,24 @@
 package project3.models;
 
+/* 
+ * MIPS has 32 registers, each one 32 bits. Decided on using an array
+ * as that is a very simple implementation and can be accessed via index
+ * i.e. register 3 is just array[3]
+ */
 public class Registers {
-    
+    private int[] registers;
+
+    // constructor
+    public Registers(){
+        registers = new int[32];
+        registers[0] = 0;
+    }
+
+    public void setRegister(int regNum, int value) {
+        registers[regNum] = value;
+    }
+
+    public int getRegister(int regNum) {
+        return registers[regNum];
+    }
 }
